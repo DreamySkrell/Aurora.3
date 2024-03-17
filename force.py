b83b1ae80111250d5efb569ad8a016fc673d1061
DreamySkrell <>
2024-03-17 17:22:28 +0100

    script

---
+++ force.py
@@ -0,0 +1,28 @@
+import os
+import re
+import pathlib
+
+def get_trailing_number(s):
+    m = re.search(r'([0-9]+[.])?[0-9]+$', s)
+    if m:
+        return float(m.group())
+    return None
+
+# regex = re.compile('(.*zip$)|(.*rar$)|(.*r01$)')
+rx = r'\tforce = (([0-9]*[.])?[0-9]+)'
+# rx = '(force)'
+
+for root, dirs, files in os.walk("D:/Git/Aurora.3"):
+    for filename in files:
+        if not filename.endswith('.dm'):
+            continue
+        file_full_path = os.path.join(root, filename)
+        # print(filename)
+        try:
+            file_str = pathlib.Path(file_full_path).read_text()
+            for res in re.finditer(rx, file_str):
+                if res.group(0):
+                    print("--",filename,'\t\t',res.group(0))
+                    print("####", get_trailing_number(res.group(0)))
+        except Exception as e:
+            continue
